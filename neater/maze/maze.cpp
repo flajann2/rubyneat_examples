f0d662c30768330acf5f3fa32a38263ebc8089da
Fred Mitchell <lordalveric@yahoo.com>
2015-05-10 17:17:16 +0200

    progress on the maze

--- neater/maze/maze.cpp
+++ neater/maze/maze.cpp
@@ -11,18 +11,101 @@ namespace maze
   Room::~Room() {
   }
 
-  Maze::Maze(int w, int b) : width(w), breadth(b) {
-    for (auto i = 0; i < w; ++i) {
-      board.insert(board.cend(), vector<Room>(b, Room()));
+  // Returns a list of all neighbors not yet
+  // visited.
+  vector<Room*> Room::available_neighbors() {
+    vector<Room*> neighbors {};
+
+    for (auto offset : neighbor_room_offsets) {
+      auto i = get<x>(offset) + get<x>(coord);
+      auto j = get<y>(offset) + get<y>(coord);
+      if (i >= 0 && j >= 0 && i < pm->width && j < pm->breadth)
+        if (! (*pm)[i][j].visited)
+          neighbors.push_back(&(*pm)[i][j]);
     }
+    return neighbors;
+  }
+
+  void Room::open_passage(Room* adjr){
+  }
+
+  void Room::dump_out() {
+    cout << "[";
+    cout << get<x>(coord) << "," << get<y>(coord) << ":";
+    for (auto wall : walls) 
+      cout << wall;
+    cout << "]";
+  }
+
+  Maze::Maze(int w, int b) : width(w), breadth(b), room_size(10.0) {
+    init();
+  }
+
+  Maze::Maze(int w, int b, double rs) : width(w),
+                                        breadth(b),
+                                        room_size(rs) {
+    init();
+  }
+
+  void Maze::init() {
+
+    for (auto i = 0; i < width; ++i) {
+      auto rooms = vector<Room>(breadth, Room());
+      for (auto j = 0; j < breadth; ++j) {
+        rooms[j].coord = make_tuple(i, j);
+        rooms[j].pm = this;
+      }
+      board.insert(board.cend(), rooms);      
+    }
+    blaze_the_maze();
+  }
+
+  void Maze::blaze_the_maze() {
+    // set up random number generation
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> rw(0, width-1);
+    uniform_int_distribution<> rb(0, breadth-1);
+    vector<Room*> pool {};
+
+    // randomly select a starting point
+    auto i = rw(gen);
+    auto j = rb(gen);
+    Room* room = &(*this)[i][j];
+    room->dump_out();
+    cout << " random beginnings at (" << i << "," << j << ")" << endl;
+    pool.push_back(room);
+
+    while(pool.size() > 0) {
+      uniform_int_distribution<> rpool(0, pool.size()-1);
+      auto rroom = rpool(gen);
+      room = pool[rroom];
+      auto neighbors = room->available_neighbors();
+      if (neighbors.size() > 0) {
+        uniform_int_distribution<> rneig(0, neighbors.size()-1);
+        auto r = rneig(gen);
+        Room* adjoining_room = neighbors[r];
+        adjoining_room->visited = true;
+        room->open_passage(adjoining_room);
+      } else {
+        auto it = pool.cbegin();
+        advance(it, rroom);
+        pool.erase(it);
+      }
+    }
+  }
+
+  vector<Room> &Maze::operator[](int i) {
+    return board[i];
   }
 
   void Maze::dump_out() {
     for (auto vw : board) {
-      cout << "vw" << endl;
+      cout << "vw: ";
       for (auto vb : vw){
-        cout << "vb ";
+        vb.dump_out();
       }
+      cout << endl;
     }
   }
 }
--- neater/maze/maze.hpp
+++ neater/maze/maze.hpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <vector>
 #include <tuple>
+#include <random>
 
 /*
   For now, we'll just do a 2-D maze.
@@ -12,19 +13,49 @@
 
 namespace maze 
 {
+  using namespace std;
+
   const int Dim = 2;
   const double room_width = 1.0;
   const double room_breadth = 1.0;
+  const int x=0;
+  const int y=1;
+  const int z=2; // for the future, we reserve this for now.
+  const vector<tuple<int, int>> neighbor_room_offsets {
+    make_tuple(-1, 0), make_tuple(1, 0),
+      make_tuple(0, -1), make_tuple(0, 1)
+      };
 
-  using namespace std;
+  /*
+    Room layout:
+    Given the maze coordinates begin in the lower left
+    corner, and progress towards the right (x) and up (y),
+    
+    * 0 - lower x
+    * 1 - upper x
+    * 2 - lower y
+    * 3 - upper y
+    (see neighbor_room_offsets)
 
-  class Room {
+    Adjacent rooms with walls knocked out MUST have
+    the adjacent wall knocked out.
+   */
+  class Room { 
+    friend class Maze;
+
   private:
     vector<bool> walls;
-    
+    bool visited;
+    tuple<int, int> coord;
+    class Maze* pm; 
+
   public:
     Room();
     ~Room();
+
+    vector<Room*> available_neighbors();
+    void open_passage(Room* adjoining_room);
+    void dump_out();
   };
   
   /* Maze class
@@ -33,17 +64,26 @@ namespace maze
      given. 
 
      In this, we shall make simplifying assumptions. 
+     * All rooms are square, all walls have zero width.
      
   */
 
   class Maze {
+    friend class Room;
+
   private:
     int width;
     int breadth;
+    double room_size;
     vector<vector<Room>> board;
     
+    void init();
+    void blaze_the_maze();
+
   public:
     Maze(int w, int b);
+    Maze(int w, int b, double room_size);
+    vector<Room> &operator[](int i);
     void dump_out();
   };
 }
