e57aca9bcd20caef7f9f2f5f15f61dc359286209
Fred Mitchell <lordalveric@yahoo.com>
2015-05-26 12:52:04 +0200

    parameters add to mz

--- lib/maze/main.cpp
+++ lib/maze/main.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 using namespace maze;
 
-int main() {
+int main(int ac, char** av) {
   cout << "Rat in the proverbial maze\n";
-  Maze m {10, 10};
+  auto width = 10;
+  auto breadth = 10;
+  if (ac == 3) {
+    width = stoi(av[1]);
+    breadth = stoi(av[2]);
+  }
+  cout << "width given is " << width << " and breadth given is " << breadth << endl;
+
+  Maze m {width, breadth};
   m.dump_out();
   return 0;
 }
