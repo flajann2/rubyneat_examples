76daf4c4fd354cfd096ff11d08c8497dd50708fa
Fred Mitchell <lordalveric@yahoo.com>
2015-05-29 10:58:43 +0200

    wall as ling segments in now

--- lib/maze.rb
+++ lib/maze.rb
@@ -33,25 +33,62 @@ module Maze
     ffi_lib MAZE_LIB
     attach_function :generate_maze, [ :int, :int ], :pointer
 
-    def gen_maze!(width, breadth)
-      @width = width
-      @breadth = breadth
-      @raw = generate_maze(@width, @breadth).get_array_of_uchar(2, @width * @breadth)
-    end
+    attr_accessor :width, :breadth, :room_measure, :wall_measure
+    attr :raw
 
-    def raw
-      @raw
+    def gen_maze!
+      @raw = generate_maze(@width, @breadth).get_array_of_uchar(2, @width * @breadth)
     end
    
     # Create the line segments that define the maze
     def wall_it!
+      bmaze = []
+      @raw.each_slice(@breadth) do |a|
+        bmaze << a
+        def a.[](i)
+          room = super
+          {
+            top:   room & (1 << 0) != 0,
+            bot:   room & (1 << 1) != 0,
+            right: room & (1 << 2) != 0,
+            left:  room & (1 << 3) != 0,
+          }
+        end
+      end
+
+      # Here we calculate the actual line segments
+      # for all the rooms
+      rmaze = []
+      roompt = @room_measure / 2.0
+      wallpt = @wall_measure / 2.0
+      (0...@width).each do |i|
+        b_rooms = []
+        x = i * @room_measure
+        (0...@breadth).each do |j|
+          room = bmaze[i][j]
+          y = j * @room_measure
+          b_rooms << {
+            top: room[:top] ? [[(x + roompt) - roompt + wallpt, (y + roompt) + roompt - wallpt], 
+                               [(x + roompt) + roompt - wallpt, (y + roompt) + roompt - wallpt]] : nil,
+            bot: room[:bot] ? [[(x + roompt) - roompt + wallpt, (y + roompt) - roompt + wallpt],
+                               [(x + roompt) + roompt - wallpt, (y + roompt) - roompt + wallpt]] : nil,
+
+            right: room[:right] ? [[(x + roompt) + roompt - wallpt, (y + roompt) - roompt + wallpt], 
+                                   [(x + roompt) + roompt - wallpt, (y + roompt) + roompt - wallpt]] : nil,
+            left:  room[:left]  ? [[(x + roompt) - roompt + wallpt, (y + roompt) - roompt + wallpt],
+                                   [(x + roompt) - roompt + wallpt, (y + roompt) + roompt - wallpt]] : nil,
+          }
+        end
+        rmaze << b_rooms
+      end
+      rmaze
     end
   end
 
   module DSL
     # dimensions of the maze (width x breadth)
     def dimensions(width: 5, breadth: 3, &block)
-      @width, @breadth = if block_given?
+      @maze.width, @maze.breadth = if block_given?
                            block.()
                          else
                            [width, breadth]
@@ -61,11 +98,11 @@ module Maze
     # Assume MKS units, define the witdth of the 
     # square room and the width of the wall.
     def physical(room: 1.0, wall: 0.1, &block)
-      @room_measure, @wall_measure = if block_given?
-                                       block.()
-                                     else
-                                       [room, wall]
-                                     end
+      @maze.room_measure, @maze.wall_measure = if block_given?
+                                                 block.()
+                                               else
+                                                 [room, wall]
+                                               end
     end
 
 
@@ -73,8 +110,13 @@ module Maze
       @maze = Maze.new
             
       def show(mazeob: @maze, &block)
-        r = mazeob.gen_maze!(@width, @breadth)
-        pp r.map{|i| i.to_s(2)}
+        r = mazeob.gen_maze!
+        mwalls = mazeob.wall_it!
+        (0...@maze.width).each do |i|
+          (0...@maze.breadth).each do |j|
+            puts "(#{i},#{j}) = #{mwalls[i][j]}"
+          end
+        end
       end
 
       block.(@maze)
