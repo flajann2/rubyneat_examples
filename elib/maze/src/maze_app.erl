b3682044559895e6095c19b406481342890479be
Fred Mitchell <lordalveric@yahoo.com>
2015-06-18 12:44:54 +0200

    in progress on erlang integration

---
+++ elib/maze/src/maze_app.erl
@@ -0,0 +1,16 @@
+-module(maze_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    maze_sup:start_link().
+
+stop(_State) ->
+    ok.
